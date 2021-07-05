e3d57c07aa7cca7cdd7bd096ed97655aec6a19c9
최창원 <choich@choechang-won-ui-MacBookPro.local>
2021-07-05 19:39:49 +0900

    Modify: Routes.js prettierr 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Nav from "./components/Nav/Nav";
-import Login from "./pages/Login/Login";
-import Main from "./pages/Main/Main";
-import Product from "./pages/Product/Product";
-import Signup from "./pages/Signup/Signup";
+import Nav from './components/Nav/Nav';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+import Product from './pages/Product/Product';
+import Signup from './pages/Signup/Signup';
 
 class Routes extends React.Component {
   render() {
