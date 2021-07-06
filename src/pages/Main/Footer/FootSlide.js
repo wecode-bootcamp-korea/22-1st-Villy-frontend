2cde56638de41d7392f0aa9ce310b9af00c14232
최창원 <choich@choechang-won-ui-MacBookPro.local>
2021-07-06 22:17:18 +0900

    ADD: 메인페이지 레이아웃 완성, 슬라이드 기능 구현(API적용 전)

--- src/pages/Main/Footer/FootSlide.js
+++ src/pages/Main/Footer/FootSlide.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BiRadioCircle, BiRadioCircleMarked } from 'react-icons/bi';
 import './FootSlide.scss';
 
 export class FootSlide extends Component {
@@ -6,21 +7,28 @@ export class FootSlide extends Component {
     super();
     this.state = {
       transValue: 0,
+      slideCtrBtnValue: 0,
       slideList: [
         {
           id: 0,
-          title: 'test1',
-          description: 'testeteset',
+          icon: 'http://localhost:3000/images/supplements.png',
+          alt: 'supplements',
+          description: 'villy 프로젝트 화이팅!!',
+          user: '도화동 사시는 김태영님',
         },
         {
           id: 1,
-          title: 'test2',
-          description: 'testeteset222',
+          icon: 'http://localhost:3000/images/vitamin.png',
+          alt: 'vitamin',
+          description: '벡엔드 화이팅!!',
+          user: '공덕동 사시는 설지우님',
         },
         {
           id: 2,
-          title: 'test3',
-          description: 'testeteset333',
+          icon: 'http://localhost:3000/images/vitamins.png',
+          alt: 'vitamins',
+          description: '프론트엔드 화이팅!!!',
+          user: '안산 사시는 최명준님',
         },
       ],
     };
@@ -33,10 +41,11 @@ export class FootSlide extends Component {
   autoSlide = () => {
     const { transValue, slideList } = this.state;
     if (transValue === (slideList.length - 1) * -100) {
-      this.setState({ transValue: 0 });
+      this.setState({ transValue: 0, slideCtrBtnValue: 0 });
     } else {
       this.setState({
         transValue: this.state.transValue - 100,
+        slideCtrBtnValue: this.state.slideCtrBtnValue + 1,
       });
     }
   };
@@ -48,7 +57,7 @@ export class FootSlide extends Component {
   };
 
   render() {
-    const { slideList, transValue } = this.state;
+    const { slideList, transValue, slideCtrBtnValue } = this.state;
     const slideWidth = slideList.length;
     let slideStyle = {
       width: slideWidth * 100 + 'vw',
@@ -61,8 +70,11 @@ export class FootSlide extends Component {
           {slideList.map(el => {
             return (
               <li key={el.id}>
-                <p>{el.title}</p>
-                <div>{el.description}</div>
+                <div>
+                  <img src={el.icon} alt={el.alt} />
+                  <h2>{el.description}</h2>
+                  <p> {el.user}</p>
+                </div>
               </li>
             );
           })}
@@ -72,8 +84,16 @@ export class FootSlide extends Component {
             {slideList.map(el => {
               return (
                 <li key={el.id}>
-                  <button onClick={this.slideHandler} value={el.id}>
-                    O
+                  <button
+                    className="slideCtr"
+                    onClick={this.slideHandler}
+                    value={el.id}
+                  >
+                    {slideCtrBtnValue === el.id ? (
+                      <BiRadioCircleMarked />
+                    ) : (
+                      <BiRadioCircle />
+                    )}
                   </button>
                 </li>
               );
