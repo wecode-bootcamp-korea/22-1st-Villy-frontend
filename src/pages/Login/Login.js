b18afea4d0dac50f31cbd98312e62f9b4ba2316c
yunkyung <poew91@gmail.com>
2021-07-06 16:40:54 +0900

    Add : Login 레이아웃 + Validation 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,21 +2,81 @@ import React from 'react';
 import './Login.scss';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userId: '',
+      userPw: '',
+      btnControl: 'disabled',
+    };
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  // 로그인 성공 여부
+  LoginFetch = () => {
+    fetch('http://10.58.5.217:8000/users/signin', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: this.state.userId,
+        password: this.state.userPw,
+      }),
+    })
+      .then(response => response.json())
+      .then(result => {
+        if (result.token !== undefined) {
+          alert('로그인 성공');
+          localStorage.setItem('access_token :', result.token);
+        } else {
+          alert('로그인 실패');
+        }
+      });
+  };
+
   render() {
+    const { userId, userPw } = this.state;
     return (
-      <div class="login_view">
-        <div class="logo">
+      <div className="login_view">
+        <div className="logo">
           <img alt="로그인 logo" src="../images/logo.png"></img>
         </div>
-        <div class="login_input">
-          <input type="text" placeholder="이메일 또는 전화번호를 입력하세요." />
-          <input type="password" placeholder="비밀번호를 입력하세요." />
-          <button type="button" className="loginButton">
-            로그인
-          </button>
-          <p>비밀번호 찾기 ㅣ 회원가입</p>
+        <div className="login_input">
+          <input
+            type="text"
+            id="userId"
+            name="userId"
+            placeholder="이메일 또는 전화번호를 입력하세요."
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            id="userPw"
+            name="userPw"
+            placeholder="비밀번호를 입력하세요."
+            onChange={this.handleChange}
+          />
+          <div className="btn">
+            <button
+              type="button"
+              className="loginButton"
+              onClick={this.LoginFetch}
+              disabled={
+                userId.includes('@') && userPw.length > 4 ? false : true
+              }
+            >
+              로그인
+            </button>
+          </div>
+          <p>
+            비밀번호 찾기 ㅣ <a href="/signup">회원가입</a>
+          </p>
         </div>
-        <div class="footer_button">
+        <div className="footer_button">
           <button type="button" className="kakao">
             KAKAO 로그인
           </button>
