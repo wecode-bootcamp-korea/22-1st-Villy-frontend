7b11b9817d0e63a4627d129fc10a000da01a4035
yunkyung <poew91@gmail.com>
2021-07-07 19:44:53 +0900

    Add : Signup 코드리뷰 반영 완료

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { chkEmail, chkPwd, chkName, chkPhone } from './Validation';
+import {
+  validationEmail,
+  validationPwd,
+  validationName,
+  validationPhone,
+} from './Validation';
 import './Signup.scss';
 
 class Signup extends React.Component {
-  emailRef = React.createRef();
-  pwRef = React.createRef();
   nameRef = React.createRef();
   phoneRef = React.createRef();
+  emailRef = React.createRef();
+  pwRef = React.createRef();
 
   state = {
     name: '',
@@ -15,56 +20,39 @@ class Signup extends React.Component {
     phone: '',
   };
 
-  emailInput = email => {
+  handleInput = e => {
+    const { name, value } = e.target;
     this.setState({
-      email: email.target.value,
-    });
-  };
-
-  passwordInput = pw => {
-    this.setState({
-      pw: pw.target.value,
-    });
-  };
-
-  nameInput = name => {
-    this.setState({
-      name: name.target.value,
-    });
-  };
-
-  phoneInput = phone => {
-    this.setState({
-      phone: phone.target.value,
+      [name]: value,
     });
   };
 
   checkAll = e => {
-    if (chkName(this.state.name) === false) {
+    if (validationName(this.state.name) === false) {
       alert('이름을 입력해주세요.');
-      // this.nameRef.current.value = '';
+      this.nameRef.current.value = '';
     }
 
-    if (chkEmail(this.state.email) === false) {
+    if (validationEmail(this.state.email) === false) {
       alert('이메일 형식이 유효하지 않습니다.');
-      // this.emailRef.current.value = '';
+      this.emailRef.current.value = '';
     }
 
-    if (chkPwd(this.state.pw) === false) {
+    if (validationPwd(this.state.pw) === false) {
       alert('영문,숫자,특수문자를 혼합하여 6~20자 이내로 입력해주세요.');
-      // this.pwRef.current.value = '';
+      this.pwRef.current.value = '';
     }
 
-    if (chkPhone(this.state.phone) === false) {
+    if (validationPhone(this.state.phone) === false) {
       alert('번호를 입력해주세요.');
-      // this.phoneRef.current.value = '';
+      this.phoneRef.current.value = '';
     }
 
     if (
-      chkName(this.state.name) &&
-      chkEmail(this.state.email) &&
-      chkPwd(this.state.pw) &&
-      chkPhone(this.state.phone) === true
+      validationName(this.state.name) &&
+      validationEmail(this.state.email) &&
+      validationPwd(this.state.pw) &&
+      validationPhone(this.state.phone)
     ) {
       alert('회원가입 완료');
     } else {
@@ -100,85 +88,100 @@ class Signup extends React.Component {
   };
 
   render() {
+    console.log(this.state);
     return (
-      <div className="signup_view">
-        <h1>회원가입</h1>
-
-        <div className="signup_name">
-          <span>이름</span>
-          <br />
-          <input
-            type="text"
-            name="name"
-            placeholder="이름을 입력해 주세요."
-            onChange={this.nameInput}
-            onKeyPress={this.handleKeyPress}
-          />
-        </div>
-        <div className="signup_phone">
-          <span>연락처</span>
-          <br />
-
-          <input
-            type="text"
-            name="phone"
-            placeholder="연락처를 입력해 주세요."
-            onChange={this.phoneInput}
-            onKeyPress={this.handleKeyPress}
-          />
-        </div>
-
-        <div className="signup_detail">
-          <span>아이디</span>
-          <br />
-          <input
-            type="text"
-            name="email"
-            ref={this.emailRef}
-            placeholder="아이디(이메일)를 입력해 주세요."
-            onChange={this.emailInput}
-            onKeyPress={this.handleKeyPress}
-          />
-          <br />
-          <span>비밀번호</span>
-          <br />
-          <input
-            type="password"
-            name="password"
-            ref={this.pwRef}
-            placeholder="비밀번호를 입력해 주세요."
-            onChange={this.passwordInput}
-            onKeyPress={this.handleKeyPress}
-          />
-          <br />
-        </div>
-        <div className="check_all">
-          <input type="checkbox"></input>
-          모두 동의하기
-        </div>
-        <div className="signup_check">
-          <input type="checkbox"></input>
-          만 14세 이상입니다.
-          <br />
-          <input type="checkbox"></input>이용 약관 동의
-        </div>
-        <div className="footer_button">
-          <button type="submit" className="signup" onClick={this.checkAll}>
-            회원가입
-          </button>
-          <br />
-          <button type="button" className="kakao">
-            KAKAO 로그인
-          </button>
-          <br />
-          <button type="button" className="facebook">
-            FACEBOOK
-          </button>
-          <br />
-          <button type="button" className="naver">
-            NAVER
-          </button>
-          <br />
+      <div className="Signup">
+        <div className="signupView">
+          <h1>회원가입</h1>
+          <div className="signupName">
+            <span className="signupHeader">이름</span>
+            <br />
+            <input
+              type="text"
+              name="name"
+              className="signupInput"
+              value={this.state.name}
+              ref={this.nameRef}
+              placeholder="이름을 입력해 주세요."
+              onChange={this.handleInput}
+              onKeyPress={this.handleKeyPress}
+            />
+          </div>
+          <div className="signup_phone">
+            <span className="signupHeader">연락처</span>
+            <br />
+            <input
+              type="text"
+              name="phone"
+              className="signupInput"
+              value={this.state.phone}
+              ref={this.phoneRef}
+              placeholder="연락처를 입력해 주세요."
+              onChange={this.handleInput}
+              onKeyPress={this.handleKeyPress}
+            />
+          </div>
+          <div className="signup_detail">
+            <span className="signupHeader">아이디</span>
+            <br />
+            <input
+              type="text"
+              name="email"
+              className="signupInput"
+              value={this.state.email}
+              ref={this.emailRef}
+              placeholder="아이디(이메일)를 입력해 주세요."
+              onChange={this.handleInput}
+              onKeyPress={this.handleKeyPress}
+            />
+            <br />
+            <span className="signupHeader">비밀번호</span>
+            <br />
+            <input
+              type="password"
+              name="password"
+              className="signupInput"
+              value={this.state.pw}
+              ref={this.pwRef}
+              placeholder="비밀번호를 입력해 주세요."
+              onChange={this.handleInput}
+              onKeyPress={this.handleKeyPress}
+            />
+            <br />
+          </div>
+          <div className="check_all">
+            <input type="checkbox" className="signupCheckbox"></input>
+            모두 동의하기
+          </div>
+          <div className="checkDetail">
+            <input type="checkbox" className="signupCheckbox"></input>
+            만 14세 이상입니다.
+            <br />
+            <input type="checkbox" className="signupCheckbox"></input>이용 약관
+            동의
+          </div>
+          <div className="footerButton">
+            <button
+              type="submit"
+              className="signupSubmit"
+              onClick={this.checkAll}
+            >
+              회원가입
+            </button>
+            <br />
+            <button type="button" className="kakao">
+              KAKAO 로그인
+            </button>
+            <br />
+            <button type="button" className="facebook">
+              FACEBOOK
+            </button>
+            <br />
+            <button type="button" className="naver">
+              NAVER
+            </button>
+            <br />
+          </div>
         </div>
       </div>
     );
