934a8e6a3636152a83d2ff38f24914be61685315
yunkyung <poew91@gmail.com>
2021-07-07 16:39:40 +0900

    Add : Login 코드리뷰 반영 수정 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.scss';
 
 class Login extends React.Component {
@@ -18,7 +19,7 @@ class Login extends React.Component {
   };
 
   // 로그인 성공 여부
-  loginFetch = () => {
+  requestLogin = () => {
     fetch('http://10.58.5.217:8000/users/signin', {
       method: 'POST',
       body: JSON.stringify({
@@ -28,9 +29,9 @@ class Login extends React.Component {
     })
       .then(response => response.json())
       .then(result => {
-        if (result.token !== undefined) {
+        if (result.token) {
           alert('로그인 성공');
-          localStorage.setItem('access_token :', result.token);
+          localStorage.setItem('access_token', result.token);
         } else {
           alert('로그인 실패');
         }
@@ -40,58 +41,65 @@ class Login extends React.Component {
   render() {
     const { userId, userPw } = this.state;
     return (
-      <div className="login_view">
-        <div className="logo">
-          <img alt="로그인 logo" src="../images/logo.png"></img>
-        </div>
-        <div className="login_input">
-          <input
-            type="text"
-            id="userId"
-            name="userId"
-            placeholder="이메일 또는 전화번호를 입력하세요."
-            onChange={this.handleChange}
-          />
-          <input
-            type="password"
-            id="userPw"
-            name="userPw"
-            placeholder="비밀번호를 입력하세요."
-            onChange={this.handleChange}
-          />
-          <div className="btn">
-            <button
-              type="button"
-              className="loginButton"
-              onClick={this.loginFetch}
-              disabled={
-                userId.includes('@') && userPw.length > 4 ? false : true
-              }
-            >
-              로그인
-            </button>
+      <div className="Login">
+        <div className="loginView">
+          <div className="loginImg">
+            <img
+              alt="logo"
+              className="loginLogo"
+              src="http://localhost:3000/images/logo.png"
+            />
           </div>
-          <p>
-            비밀번호 찾기 ㅣ <a href="/signup">회원가입</a>
-          </p>
-        </div>
-        <div className="footer_button">
-          <button type="button" className="kakao">
-            KAKAO 로그인
-          </button>
-          <br />
-          <button type="button" className="facebook">
-            FACEBOOK
-          </button>
-          <br />
-          <button type="button" className="naver">
-            NAVER
-          </button>
-          <br />
-          <button type="button" className="apple">
-            APPLE
-          </button>
-          <br />
+          <div className="loginForm">
+            <input
+              type="text"
+              name="userId"
+              className="loginInput"
+              placeholder="이메일 또는 전화번호를 입력하세요."
+              onChange={this.handleChange}
+            />
+            <input
+              type="password"
+              name="userPw"
+              className="loginInput"
+              placeholder="비밀번호를 입력하세요."
+              onChange={this.handleChange}
+            />
+            <div className="btn">
+              <button
+                type="button"
+                className="loginButton"
+                onClick={this.requestLogin}
+                disabled={!(userId.includes('@') && userPw.length > 4)}
+              >
+                로그인
+              </button>
+            </div>
+            <p className="loginText">
+              비밀번호 찾기 ㅣ
+              <Link className="loginFormLink" to="/signup">
+                회원가입
+              </Link>
+            </p>
+          </div>
+          <div className="footerButton">
+            <button type="button" className="kakao">
+              KAKAO 로그인
+            </button>
+            <br />
+            <button type="button" className="facebook">
+              FACEBOOK
+            </button>
+            <br />
+            <button type="button" className="naver">
+              NAVER
+            </button>
+            <br />
+            <button type="button" className="apple">
+              APPLE
+            </button>
+            <br />
+          </div>
         </div>
       </div>
     );
