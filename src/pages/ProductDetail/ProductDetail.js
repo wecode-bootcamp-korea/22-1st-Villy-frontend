ba1a8110838532a3d0edf123b14edd8dbcf0eb07
최창원 <choich@choechang-won-ui-MacBookPro.local>
2021-07-08 15:23:36 +0900

    ADD: 데이터를 통해 백그라운드 변경 구현

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -6,6 +6,17 @@ export class ProductDetail extends Component {
     super();
     this.state = {
       productData: [],
+      backgroundColor: {
+        1: '#E9F9FE',
+        2: '#E0B5BA',
+        3: '#EFE9D9',
+        4: '#B1D9F1',
+        5: '#EAE9E9',
+        6: '#AEE19E',
+        7: '#FFF0A6',
+        8: '#CBC5E8',
+        9: '#FAD4BF',
+      },
     };
   }
 
@@ -20,30 +31,34 @@ export class ProductDetail extends Component {
         });
       });
   }
+
   render() {
-    console.log(`this.state`, this.state);
-    return (
-      <div className="ProductDetail">
-        <main>
-          <div className="headTextWrap">
-            <h1>
-              내 몸에 필요한
-              <br />
-              {/* 필수 영양소{porouctData[0].name} */}
-            </h1>
-            <p>
-              나만을 위한 맞춤비타민을 찾아보세요!
-              <br /> 이미 <strong>587,064명</strong>이 추천받았습니다.
-            </p>
-            <button>지금 시작하기</button>
-          </div>
-        </main>
-        <section className="comment">
-          <h2>그동안 고민 많았죠?</h2>
-        </section>
-      </div>
-    );
+    if (!this.state.productData[0]) {
+      return <div>..Loading</div>;
+    } else {
+      const { productData, backgroundColor } = this.state;
+      return (
+        <div className="ProductDetail">
+          <main style={{ backgroundColor: backgroundColor[productData[0].id] }}>
+            <div className="headTextWrap">
+              <h1>
+                {productData[0].efficacy}
+                <br />
+                {productData[0].name}
+              </h1>
+              <p>
+                나만을 위한 맞춤비타민을 찾아보세요!
+                <br /> 이미 <strong>587,064명</strong>이 추천받았습니다.
+              </p>
+              <button>지금 시작하기</button>
+            </div>
+          </main>
+          <section className="comment">
+            <h2>그동안 고민 많았죠?</h2>
+          </section>
+        </div>
+      );
+    }
   }
 }
-
 export default ProductDetail;
