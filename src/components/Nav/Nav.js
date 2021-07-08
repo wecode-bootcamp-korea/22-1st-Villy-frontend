b93ec38d56f0f269a074be24b34bb10f8dac6daa
yunkyung <poew91@gmail.com>
2021-07-08 23:55:11 +0900

    3차 코드리뷰 수정 완료 (Link tag를 map으로 구현)

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,35 +3,36 @@ import { Link } from 'react-router-dom';
 import './Nav.scss';
 
 class Nav extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      menuList: [
+        { name: '추천상품', link: '/login' },
+        { name: '제품보기', link: '/product' },
+        { name: '고객후기', link: '/login' },
+        { name: '장바구니', link: '/cart' },
+        { name: '로그인', link: '/login' },
+      ],
+    };
+  }
+
   render() {
     return (
       <nav className="navbar">
         <div className="navLogo">
           <Link to="/login">
-            <img
-              alt="logo"
-              className="logoImg"
-              src="http://localhost:3000/images/logo.png"
-            />
+            <img alt="logo" className="logoImg" src="/images/logo.png" />
           </Link>
         </div>
         <ul className="navMenu">
           <li className="navList">
-            <Link className="navLink" to="">
-              추천성분
-            </Link>
-            <Link className="navLink" to="/product">
-              제품보기
-            </Link>
-            <Link className="navLink" to="">
-              고객후기
-            </Link>
-            <Link className="navLink" to="">
-              장바구니
-            </Link>
-            <Link className="navLink" to="/login">
-              로그인
-            </Link>
+            {this.state.menuList.map((menu, i) => {
+              return (
+                <Link className="navLink" to={menu.link} key={i}>
+                  {menu.name}
+                </Link>
+              );
+            })}
           </li>
         </ul>
       </nav>
