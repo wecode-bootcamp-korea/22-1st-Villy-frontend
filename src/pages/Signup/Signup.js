e687141fdd2fbdc803307b482e5c62ac479f2954
yunkyung <poew91@gmail.com>
2021-07-09 18:25:11 +0900

    3차 코드리뷰 수정 완료 (scss에 nth-of-type 사용해서 리팩토링)

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { validationFor } from './Validation';
-import { POST_SIGNUP_API } from '../../../src/config.js';
+// import { POST_SIGNUP_API } from '../../../src/config.js';
 import './Signup.scss';
 
 class Signup extends React.Component {
@@ -19,102 +19,83 @@ class Signup extends React.Component {
     });
   };
 
-  // Validation을 체크하는 함수
-  doValidation = e => {
-    const userInputs = Object.entries(this.state);
-    console.log(userInputs);
-    userInputs.forEach(el => {
-      console.log(validationFor[el[0]](el[1]));
-      if (!validationFor[el[0]](el[1])) {
-        return alert('양식에 맞지 않습니다');
-      }
-    });
+  // Validation을 체크하는 함수 (구현중...)
+  // doValidation = e => {
+  //   const userInputs = Object.entries(this.state);
+  //   console.log(userInputs);
+  //   userInputs.forEach(el => {
+  //     console.log(validationFor[el[0]](el[1]));
+  //     if (!validationFor[el[0]](el[1])) {
+  //       return alert('양식에 맞지 않습니다');
+  //     }
+  // });
 
-    // if (!validationName(this.state.name)) {
-    //   alert('이름을 입력해주세요.');
-    // }
-    // if (!validationMobile(this.state.mobile)) {
-    //   alert('번호를 입력해주세요.');
-    // }
-    // if (!validationEmail(this.state.email)) {
-    //   alert('이메일 형식이 유효하지 않습니다.');
-    // }
-    // if (!validationPwd(this.state.pw)) {
-    //   alert('영문,숫자,특수문자를 혼합하여 6~20자 이내로 입력해주세요.');
-    // }
-    // if (
-    //   validationName(this.state.name) &&
-    //   validationMobile(this.state.mobile) &&
-    //   validationEmail(this.state.email) &&
-    //   validationPwd(this.state.pw)
-    // ) {
-    //   return true;
-    //   // alert('회원가입 완료');
-    // } else {
-    //   alert('입력란을 다시 확인하세요.');
-    //   return false;
-    // }
-  };
+  // 리팩토링 이전 Validation 함수
+  // if (!validationName(this.state.name)) {
+  //   alert('이름을 입력해주세요.');
+  // }
+  // if (!validationMobile(this.state.mobile)) {
+  //   alert('번호를 입력해주세요.');
+  // }
+  // if (!validationEmail(this.state.email)) {
+  //   alert('이메일 형식이 유효하지 않습니다.');
+  // }
+  // if (!validationPwd(this.state.pw)) {
+  //   alert('영문,숫자,특수문자를 혼합하여 6~20자 이내로 입력해주세요.');
+  // }
+  // if (
+  //   validationName(this.state.name) &&
+  //   validationMobile(this.state.mobile) &&
+  //   validationEmail(this.state.email) &&
+  //   validationPwd(this.state.pw)
+  // ) {
+  //   return true;
+  //   // alert('회원가입 완료');
+  // } else {
+  //   alert('입력란을 다시 확인하세요.');
+  //   return false;
+  // }
+  // };
 
   // Back이랑 연결하는 fetch 함수
-  requestSignup = () => {
-    fetch(`${POST_SIGNUP_API}`, {
-      method: 'POST',
-      body: JSON.stringify({
-        name: this.state.name,
-        mobile: this.state.mobile,
-        email: this.state.email,
-        password: this.state.pw,
-      }),
-    })
-      .then(res => res.json())
-      .then(res => {
-        if (res.access_token) {
-          alert('회원가입 성공');
-          localStorage.setItem('access_token', res.access_token);
-        } else {
-          alert('회원가입 실패');
-        }
-      });
-  };
-
-  // 입력 완료 후 실행되는 함수
-  handleKeyPress = e => {
-    if (e.key === 'Enter') {
-      this.requestSignup();
-    }
-    // if (e.key === 'Enter' && this.doValidation()) {
-    //   return this.requestSignup();
-    // }
-  };
-
   // requestSignup = () => {
-  //   fetch('http://10.58.6.222:8000/users/signup', {
+  //   fetch(`${POST_SIGNUP_API}`, {
   //     method: 'POST',
   //     body: JSON.stringify({
   //       name: this.state.name,
-  //       phone: this.state.phone,
+  //       mobile: this.state.mobile,
   //       email: this.state.email,
   //       password: this.state.pw,
   //     }),
   //   })
   //     .then(res => res.json())
-  //     .then(res_success => {
-  //       if (res_success) {
+  //     .then(res => {
+  //       if (res.access_token) {
   //         alert('회원가입 성공');
-  //         localStorage.setItem('access_token', res_success);
+  //         localStorage.setItem('access_token', res.access_token);
   //       } else {
   //         alert('회원가입 실패');
   //       }
   //     });
   // };
 
+  // 입력 완료 후 실행되는 함수
+  handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      this.requestSignup();
+    }
+    // (구현중...)
+    // if (e.key === 'Enter' && this.doValidation()) {
+    //   return this.requestSignup();
+    // }
+  };
+
   render() {
     return (
       <div className="Signup">
         <div className="signupView">
           <h1>회원가입</h1>
-          <div className="signupName">
+          <div className="signupInputWrapper">
             <span className="signupHeader">이름</span>
             <br />
             <input
@@ -127,7 +108,7 @@ class Signup extends React.Component {
               onKeyPress={this.handleKeyPress}
             />
           </div>
-          <div className="signupPhone">
+          <div className="signupInputWrapper">
             <span className="signupHeader">연락처</span>
             <br />
             <input
@@ -140,7 +121,7 @@ class Signup extends React.Component {
               onKeyPress={this.handleKeyPress}
             />
           </div>
-          <div className="signupDetail">
+          <div className="signupInputWrapper">
             <span className="signupHeader">아이디</span>
             <br />
             <input
@@ -153,6 +134,8 @@ class Signup extends React.Component {
               onKeyPress={this.handleKeyPress}
             />
             <br />
+          </div>
+          <div className="signupInputWrapper">
             <span className="signupHeader">비밀번호</span>
             <br />
             <input
@@ -163,11 +146,10 @@ class Signup extends React.Component {
               placeholder="비밀번호를 입력해 주세요."
               onKeyPress={this.handleKeyPress}
               onChange={this.handleInput}
-              onKeyPress={this.handleKeyPress}
             />
-            <br />
           </div>
-          {/* <div className="check_all">
+          {/* // 추가로 구현할 예정...
+          <div className="check_all">
             <input type="checkbox" className="signupCheckbox"></input>
             모두 동의하기
           </div>
