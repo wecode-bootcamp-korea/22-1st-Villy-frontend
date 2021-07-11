ee8403d9b672c2f1f487136e4741ec0295a2dde0
ChangWon Choi <choiuhan@gmail.com>
2021-07-11 14:56:24 +0900

    --allow-empty-message

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const BASE_URL = 'http://localhost:3000/';
+const BASE_URL = 'http://localhost:3000';
 
 export const POST_SIGNUP_API = `${BASE_URL}/users/signup`;
 export const POST_SIGNIN_API = `${BASE_URL}/users/signin`;
-export const GET_PRODUCTS_API = `${BASE_URL}/products`;
-export const GET_PRODUCTS_EFFICACY_API = `${BASE_URL}/data/products`;
+export const GET_PRODUCTS_API = `${BASE_URL}/data/products`;
+export const POST_ADD_CART_API = `${BASE_URL}/carts`;
