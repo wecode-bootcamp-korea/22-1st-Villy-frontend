f3a820f560548609fdff0fdf97b5cd57577be591
soojeonglee <eee.soojeong@gmail.com>
2021-07-13 00:15:00 +0900

    [Add] ProductCategory 컴포넌트 생성

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { GET_PRODUCTS_API } from '../../config';
+// import { GET_PRODUCTS_API } from '../../config';
 import ProductCard from './ProductCard/ProductCard';
 import ProductCategory from './ProductCategory/ProductCategory';
 
@@ -12,12 +12,6 @@ export class Product extends Component {
     super();
     this.state = {
       productCard: [],
-      filterState: {
-        bone: false,
-        hair: false,
-        growth: false,
-        skin: false,
-      },
     };
   }
 
@@ -31,64 +25,6 @@ export class Product extends Component {
         });
       });
   }
-
-  makeCondition = () => {
-    // const filterState = {
-    //   bone: false,
-    //   hair: true,
-    //   growth: true,
-    //   skin: false,
-    // };
-
-    const filterMatch = {
-      bone: 1,
-      hair: 2,
-      growth: 3,
-      skin: 4,
-    };
-
-    const filtered = Object.entries(this.state.filterState).reduce(
-      (acc, [key, value]) => {
-        //첫번째 조건도 필요함 & 없을때
-        if (!acc && value) {
-          return acc + `efficacy=${filterMatch[key]}`;
-        }
-
-        if (value) {
-          return acc + `&efficacy=${filterMatch[key]}`;
-        }
-        return acc;
-      },
-      ''
-    );
-
-    // fetch(`./data/ProductData.json${filtered}`)
-    fetch(`${GET_PRODUCTS_API}?${filtered}`)
-      .then(res => res.json())
-      .then(data => {
-        this.setState({
-          productCard: data.message,
-        });
-      });
-
-    console.log(filtered);
-    // console.log(this.state.filterState);
-  };
-
-  handleCheckBox = event => {
-    const checkBoxName = event.target.name;
-    const tf = !this.state.filterState[checkBoxName];
-
-    this.setState(
-      {
-        filterState: { ...this.state.filterState, [checkBoxName]: tf },
-      },
-      () => {
-        this.makeCondition();
-      }
-    );
-    console.log(`this.state.filterState`, this.state.filterState);
-  };
 
   render() {
     const { productCard } = this.state;
@@ -107,52 +43,6 @@ export class Product extends Component {
 
           <form className="productCategory">
             <ProductCategory />
-
-            {/* <div className="ProductCheckBox">
-              <input
-                type="checkbox"
-                name="hair"
-                onChange={this.handleCheckBox}
-              />
-              <label>
-                <img
-                  className="iconImage"
-                  alt="hair"
-                  src="/images/hairstyle.svg"
-                />
-                <span className="categoryText">모발</span>
-              </label>
-            </div>
-            <div className="ProductCheckBox">
-              <input
-                type="checkbox"
-                name="growth"
-                onChange={this.handleCheckBox}
-              />
-              <label>
-                <img
-                  className="iconImage"
-                  alt="growth"
-                  src="/images/height.svg"
-                />
-                <span className="categoryText">성장</span>
-              </label>
-            </div>
-            <div className="ProductCheckBox">
-              <input
-                type="checkbox"
-                name="skin"
-                onChange={this.handleCheckBox}
-              />
-              <label>
-                <img
-                  className="iconImage"
-                  alt="skin"
-                  src="/images/therapy.svg"
-                />
-                <span className="categoryText">피부</span>
-              </label>
-            </div> */}
           </form>
 
           <ul className="productList">
--- src/pages/Product/ProductCategory/ProductCategory.js
+++ src/pages/Product/ProductCategory/ProductCategory.js
@@ -1,21 +1,116 @@
 import React, { Component } from 'react';
+// import { GET_PRODUCTS_API } from '../../config';
 
 export class ProductCategory extends Component {
-  render() {
-    return (
-      <div className="ProductCheckBox">
-        <input
-          type="checkbox"
-          name="bone"
-          onChange={this.props.handleCheckBox}
-        />
-        <label>
-          <img className="iconImage" alt="bone" src="/images/bone.svg" />
-          <span className="categoryText">뼈</span>
-        </label>
-      </div>
+  constructor() {
+    super();
+    this.state = {
+      filterState: {
+        bone: false,
+        hair: false,
+        growth: false,
+        skin: false,
+      },
+    };
+  }
+
+  makeCondition = () => {
+    // 비동기 문제 있음
+    // 패치를 위한 함수를 만들어야할까?
+    const filterMatch = {
+      bone: 1,
+      hair: 2,
+      growth: 3,
+      skin: 4,
+    };
+
+    const filtered = Object.entries(this.state.filterState).reduce(
+      (acc, [key, value]) => {
+        if (!acc && value) {
+          return acc + `efficacy=${filterMatch[key]}`;
+        }
+
+        if (value) {
+          return acc + `&efficacy=${filterMatch[key]}`;
+        }
+
+        return acc;
+      },
+      ''
     );
+
+    // fetch(`${GET_PRODUCTS_API}?${filtered}`)
+    fetch(`./data/ProductData.json${filtered}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          productCard: data.message,
+        });
+      });
+  };
+
+  handleCheckBox = event => {
+    const checkBoxName = event.target.name;
+    const tf = !this.state.filterState[checkBoxName];
+
+    this.setState(
+      {
+        filterState: { ...this.state.filterState, [checkBoxName]: tf },
+      },
+      () => {
+        this.makeCondition();
+      }
+    );
+    console.log(`this.state.filterState`, this.state.filterState);
+  };
+
+  render() {
+    return PRODUCT_CATEGORY.map(category => {
+      return (
+        <div className="ProductCheckBox" key={category.id}>
+          <input
+            type="checkbox"
+            name={category.name}
+            onChange={this.handleCheckBox}
+          />
+          <label>
+            <img className="iconImage" alt={category.alt} src={category.src} />
+            <span className="categoryText">{category.categoryText}</span>
+          </label>
+        </div>
+      );
+    });
   }
 }
 
+const PRODUCT_CATEGORY = [
+  {
+    id: 1,
+    name: 'bone',
+    alt: 'bone',
+    src: '/images/bone.svg',
+    categoryText: '뼈',
+  },
+  {
+    id: 2,
+    name: 'hair',
+    alt: 'hair',
+    src: '/images/hairstyle.svg',
+    categoryText: '모발',
+  },
+  {
+    id: 3,
+    name: 'growth',
+    alt: 'growth',
+    src: '/images/height.svg',
+    categoryText: '성장',
+  },
+  {
+    // id: 4,
+    name: 'skin',
+    alt: 'skin',
+    src: '/images/therapy.svg',
+    categoryText: '피부',
+  },
+];
 export default ProductCategory;
