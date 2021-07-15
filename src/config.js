f9e3715ea6bee8b660171233e446623c45a82260
soojeonglee <eee.soojeong@gmail.com>
2021-07-15 16:03:50 +0900

    [Fix] config URL 통일

--- src/config.js
+++ src/config.js
@@ -3,3 +3,5 @@ const BASE_URL = 'http://3.35.18.96:8000';
 export const POST_SIGNUP_API = `${BASE_URL}/users/signup`;
 export const POST_SIGNIN_API = `${BASE_URL}/users/signin`;
 export const GET_PRODUCTS_API = `${BASE_URL}/products`;
+export const CARTLIST_API = `${BASE_URL}/carts`;
+export const ORDER_API = `${BASE_URL}/orders`;
