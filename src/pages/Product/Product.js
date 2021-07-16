845fe097f2eb8cc5d72f5f95ac40a1609c25e37a
ChangWon Choi <choiuhan@gmail.com>
2021-07-16 10:48:42 +0900

    제품 상세페이지, 전체 글씨 css변경

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -31,7 +31,9 @@ export class Product extends Component {
 
   componentDidMount() {
     if (this.props.history.location.search) {
-      fetch(`${GET_PRODUCTS_API}${this.props.history.location.search}`)
+      fetch(`${GET_PRODUCTS_API}${this.props.history.location.search}`, {
+        headers: { Authorization: localStorage.getItem('access_token') },
+      })
         .then(res => res.json())
         .then(data => {
           this.setState({
@@ -39,7 +41,9 @@ export class Product extends Component {
           });
         });
     } else
-      fetch(`${GET_PRODUCTS_API}`)
+      fetch(`${GET_PRODUCTS_API}`, {
+        headers: { Authorization: localStorage.getItem('access_token') },
+      })
         .then(res => res.json())
         .then(data => {
           this.setState({
